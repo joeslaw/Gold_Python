84e21cd084e8f4a445ebe24b44b16edfe1950bba
joeslaw <jslawinski92@gmail.com>
2022-11-27 20:48:29 -0500

    Terraform 20 part 2

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -1,15 +1,43 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 2.23.0"
     }
   }
 }
 
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+resource "random_string" "random2" {
+  length  = 4
+  special = false
+  upper   = false
+}
 provider "docker" {}
 
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
+}
+
+resource "docker_container" "nodered_container" {
+  name  = join("-", ["nodered", random_string.random.result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    #external = 1880
+  }
+}
+
+resource "docker_container" "nodered_container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    #external = 1880
+  }
 }
