2e23c2bda92a7d6b75c751a40baab4b783ff871e
joeslaw <jslawinski92@gmail.com>
2022-11-29 18:17:09 -0500

    Terraform Commit

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -8,15 +8,12 @@ terraform {
 }
 
 resource "random_string" "random" {
+  count = 2  
   length  = 4
   special = false
   upper   = false
 }
-resource "random_string" "random2" {
-  length  = 4
-  special = false
-  upper   = false
-}
+
 provider "docker" {}
 
 
@@ -25,7 +22,8 @@ resource "docker_image" "nodered_image" {
 }
 
 resource "docker_container" "nodered_container" {
-  name  = join("-", ["nodered", random_string.random.result])
+  count = 2  
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
@@ -33,11 +31,14 @@ resource "docker_container" "nodered_container" {
   }
 }
 
-resource "docker_container" "nodered_container2" {
-  name  = join("-", ["nodered", random_string.random2.result])
-  image = docker_image.nodered_image.latest
-  ports {
-    internal = 1880
-    #external = 1880
-  }
+output "ip-address" {
+    value = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+    description = "The IP address and external port of the container"
 }
+
+output "container-name" {
+    value = docker_container.nodered_container[*].name
+    description = "The name of the container"
+
+}
+
