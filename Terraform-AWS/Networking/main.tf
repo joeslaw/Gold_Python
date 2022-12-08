3d5b5c044c9d62c6a741ca4778157593e98ea6ba
joeslaw <jslawinski92@gmail.com>
2022-12-07 22:28:20 -0500

    Terraformwk22#3

--- Terraform-AWS/Networking/main.tf
+++ Terraform-AWS/Networking/main.tf
@@ -84,3 +84,26 @@ resource "aws_default_route_table" "private_rt" {
     Name = "privatert"
   }
 }
+
+resource "aws_security_group" "sg" {
+  for_each    = var.security_groups
+  name        = each.value.name
+  description = each.value.description
+  vpc_id      = aws_vpc.mtc_vpc.id
+  dynamic "ingress" {
+    for_each = each.value.ingress
+    content {
+      from_port   = ingress.value.from
+      to_port     = ingress.value.to
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
